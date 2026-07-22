60fa6316fd71d1a71b80c443b735aeb8fe096b06
Rakib Hasan Purno <your-email@example.com>
2026-07-22 12:51:49 +0600

    Admin Meta Name

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -1,6 +1,6 @@
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>Trace Consulting</title>
+<title>TRACE Consulting</title>
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="abc">
